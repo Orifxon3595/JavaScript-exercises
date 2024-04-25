cff798c44243d9cafe94c44048f4681df41cf3ea
Orifxon3595 <orifxontuychiyev@gmail.com>
2024-04-25 17:07:19 +0500

    first commit

---
+++ 1-dars/index.js
@@ -0,0 +1,25 @@
+// Rewrite if...else using multiple ternary operators "?"
+
+// let message;
+
+// if(login == 'Employee') {
+//     message == 'Hello';
+// } else if(login == 'Director') {
+//     message = 'Greetings';
+// } else if(login == '') {
+//     message = 'No login';
+// } else {
+//     message = '';
+// }
+
+
+// Answer
+let message;
+
+login == 'Employee'
+? message = 'Hello'
+: login == 'Director'
+? message = 'Greettings'
+: login == ''
+? message = 'No login'
+: message = ''
